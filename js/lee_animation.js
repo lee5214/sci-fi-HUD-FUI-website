2cc5246ab2ab467c8afa7ad76de7655f313ae756
lee5214 <lee5214@hotmail.com>
2016-07-06 16:05:13 -0700

    about

--- js/lee_animation.js
+++ js/lee_animation.js
@@ -13,4 +13,6 @@ $("button").click(function(){
         $(".hud_panel_l1").toggleClass("hud_panel_l1_remove")
         $(".hud_panel_l2").toggleClass("hud_panel_l2_remove")
         $(".hud_panel_l3").toggleClass("hud_panel_l3_remove")
+        $(".hud_panel_l4").toggleClass("hud_panel_l4_in")
+        //$(".hud_panel_l4").addClass("shanshuo2").addClass("biankuang_1")
 });
