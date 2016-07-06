a4662a755937ab1610be20955914bed5c8a4bf7f
lee5214 <lee5214@hotmail.com>
2016-07-06 14:26:52 -0700

    small

--- js/lee_animation.js
+++ js/lee_animation.js
@@ -1,8 +1,16 @@
 /**
  * Created by Administrator on 2016/6/16.
  */
+/*
 var spin = document.createElement('div');
 var line1 = document.createElement('div');
 line1.src = "resource\animation\headline.png";
 line1.appendChild(spin);
 document.getElementsByClassName("center-animation").appendChild('spin');
+*/
+
+$("button").click(function(){
+        $(".hud_panel_l1").toggleClass("hud_panel_l1_remove")
+        $(".hud_panel_l2").toggleClass("hud_panel_l2_remove")
+        $(".hud_panel_l3").toggleClass("hud_panel_l3_remove")
+});
