34eab935d06a762239b6e2b77797d03935b07812
lee5214 <lee5214@hotmail.com>
2016-07-13 20:44:50 -0700

    content fix

--- js/lee_animation.js
+++ js/lee_animation.js
@@ -9,10 +9,14 @@ line1.appendChild(spin);
 document.getElementsByClassName("center-animation").appendChild('spin');
 */
 
-$("button").click(function(){
-        $(".hud_panel_l1").toggleClass("hud_panel_l1_remove")
-        $(".hud_panel_l2").toggleClass("hud_panel_l2_remove")
-        $(".hud_panel_l3").toggleClass("hud_panel_l3_remove")
-        $(".hud_panel_l4").toggleClass("hud_panel_l4_in")
-        //$(".hud_panel_l4").addClass("shanshuo2").addClass("biankuang_1")
+$("button#about").click(function(){
+    $(".hud_panel_l1").slideToggle(1000)
+    $(".hud_panel_l2").delay(400).slideToggle(400)
+    $(".hud_panel_l3").delay(600).slideToggle(400)
+    $(".hud_panel_l4").delay(800).slideToggle(400)
+    //$(".hud_panel_l4").delay(1000).slideToggle(400)
+        //$(".hud_panel_l1").toggleClass("hud_panel_l1_remove")
+       // $(".hud_panel_l2").toggleClass("hud_panel_l2_remove")
+      //  $(".hud_panel_l3").toggleClass("hud_panel_l3_remove")
+    //$(".hud_panel_l4").toggleClass("hud_panel_l4_in")
 });
